5cd621823ef4c03488d9655e35eac6fa21709c25
前田 <sloth19920731@gmail.com>
2022-01-26 23:00:07 +0900

    Bookクラス作成まで

--- src/main/resources/webapp/bookstore-app/src/app/app.module.ts
+++ src/main/resources/webapp/bookstore-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBookComponent } from './book/add-book/add-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent
   ],
   imports: [
     BrowserModule,
